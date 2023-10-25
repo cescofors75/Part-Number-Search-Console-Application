34adc0f9fc10e86b66552e050d03701c25429d48
Cesco Fors <88708847+cescofors75@users.noreply.github.com>
2023-10-25 12:18:17 +0200

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -15,20 +15,20 @@ using ConsoleApp3.Models;
     bool exit = false;
     do
     {
-        Console.Write("Enter Euro4x4 part number: ");
+        Console.Write("Enter  part number: ");
         string reference = Console.ReadLine();
         Console.Clear();
         Console.WriteLine();
 
         if (reference == "list") {
-            Console.WriteLine("List Euro4x4 part number stocked:");
+            Console.WriteLine("Listpart number stocked:");
             metodo.PrintRefStocked();
             
         }
 
         if (reference == "ean")
         {
-            Console.WriteLine("List EAN from Euro4x4 part number: ");
+            Console.WriteLine("List EAN from  part number: ");
             
             metodo.PrintRefEuroEan();
             
@@ -43,7 +43,7 @@ using ConsoleApp3.Models;
         {
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Black;
-            Console.WriteLine("Euro4x4 part number stocked");
+            Console.WriteLine(" part number stocked");
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             metodo.PrintStores(reference);
@@ -52,7 +52,7 @@ using ConsoleApp3.Models;
         {
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Green;
-            Console.WriteLine("New Euro4x4 part number to stocked yours stores");
+            Console.WriteLine("New part number to stocked yours stores");
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             metodo.SearchEan(reference);
